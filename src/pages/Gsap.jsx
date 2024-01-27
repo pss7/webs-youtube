8222d22522feec726cccc589b60ed73ec6d2f693
Username <parkss0157@naver.com>
2024-01-28 04:28:03 +0900

    페이지 SEO 작업

--- src/pages/Gsap.jsx
+++ src/pages/Gsap.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Main from '../components/section/Main';
 
 const Gasp = () => {
-  return (
-    <Main>
-      Gasp
-    </Main>
-  )
+    return (
+        <Main
+            title="GSAP 사이트"
+            description="GSAP 사이트 튜토리얼 강의입니다.">
+            Gsap
+        </Main>
+    )
 }
 
 export default Gasp
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import Main from '../components/section/Main';
 
 const Home = () => {
     return (
-        <Main />
+        <Main title="유튜브 메인"
+            description="유튜브에 오신 것을 환영합니다.">
+            Home
+        </Main>
     )
 }
 
