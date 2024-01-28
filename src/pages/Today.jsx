5d4e044f58710434aec93c4f610eeeda92aaf11f
Username <parkss0157@naver.com>
2024-01-28 14:16:39 +0900

    추천 영상 작업

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import Main from '../components/section/Main';
+import { todayText } from '../data/today';
+import { Link } from 'react-router-dom'
 
 const Today = () => {
     return (
         <Main
             title="추천 영상"
             description="오늘의 추천 유튜브 영상입니다.">
-            Today
+            <section id='todayPage'>
+                <h2>
+                    오늘의 추천 영상입니다.
+                </h2>
+                {
+                    todayText.map((today, key) => (
+                        <div className='today__inner' key={key}>
+                            <div className='today__thumb play__icon'>
+                                <Link to={today.page}>
+                                    <img src={today.img} alt={today.title} />
+                                </Link>
+                            </div>
+                            <div className='today__text'>
+                                <span className='today'>
+                                    today!
+                                </span>
+                                <h3 className='title'>
+                                    <Link to={today.page}>
+                                        {today.title}
+                                    </Link>
+                                </h3>
+                                <p className='desc'>{today.desc}</p>
+                                <div className='info'>
+                                    <Link to={`/channel/${today.channelId}`}>
+                                        <span className='author'>{today.author}</span>
+                                    </Link>
+                                    <span className='date'>{today.date}</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </section>
+
         </Main>
     )
 }
