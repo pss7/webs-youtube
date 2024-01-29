fcffa31b4907b2ad9f567112fb311f265b18ed40
Username <parkss0157@naver.com>
2024-01-29 21:50:51 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/components/contents/Gsap.jsx
+++ src/components/contents/Gsap.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { gsapText } from '../../data/gsap.js';
+import { Link } from 'react-router-dom';
 
 const Gsap = () => {
   return (
-    <div>Gsap</div>
+    <section id='Gsap'>
+      <h2>GSAP 패럴랙스 효과를 하고 싶다면!</h2>
+      <div className="video__inner">
+        {
+          gsapText.map((video, key) => (
+            <div className='video' key={key}>
+              <div className='video__thumb play__icon'>
+                <Link to={`/video/${video.videoId}`}>
+                  <img src={video.img} alt={video.title} />
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
   )
 }
 
--- src/components/contents/Portfolio.jsx
+++ src/components/contents/Portfolio.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { portfolioText } from '../../data/portfolio.js';
+import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h2>포트폴리오 만드는 방법을 공유합니다.</h2>
+      <div className="video__inner">
+        {
+          portfolioText.map((video, key) => (
+            <div className='video' key={key}>
+              <div className='video__thumb play__icon'>
+                <Link to={`/video/${video.videoId}`}>
+                  <img src={video.img} alt={video.title} />
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
   )
 }
 
