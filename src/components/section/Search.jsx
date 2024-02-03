a84e9b33e2711ec10947bde91e8140a58ab1e6cc
Username <parkss0157@naver.com>
2024-02-03 19:44:47 +0900

    Rapid API 및 더보기 설정하기

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-    const [searchKeyword, setSearchKeyword] = useState('');
+    const [ searchKeyword, setSearchKeyword ] = useState('');
     const navigate = useNavigate();
 
     const handleSearch = () => {
-        console.log(searchKeyword)
-        if (searchKeyword) {
+        console.log(searchKeyword);
+        if(searchKeyword){
             navigate(`/search/${searchKeyword}`);
             setSearchKeyword('');
         }
-    };
+    }
 
     return (
         <div id='search'>
@@ -19,15 +19,16 @@ const Search = () => {
                 <label htmlFor='searchInput'>
                     <span className='ir'>검색</span>
                 </label>
-                <input
-                    type='search'
-                    id='searchInput'
-                    placeholder='검색어를 입력해주세요'
+                <input 
+                    type='search' 
+                    name='searchInput' 
+                    id='searchInput' 
                     autoComplete='off'
                     className='search__input'
+                    placeholder='검색어를 입력해주세요!'
                     onChange={e => setSearchKeyword(e.target.value)}
                     onKeyDown={e => {
-                        if (e.key === 'Enter') {
+                        if(e.key === 'Enter'){
                             handleSearch();
                         }
                     }}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,31 +3,53 @@ import { useParams } from 'react-router-dom'
 import Main from '../components/section/Main'
 
 import VideoSearch from '../components/video/VideoSearch'
+import { fetchFromAPI } from '../utils/api'
 
 const Search = () => {
     const { searchId } = useParams();
     const [videos, setVideos] = useState([]);
+    const [nextPageToken, setNextPageToken] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-        )
-            .then(response => response.json())
-            .then(result => {
-                console.log(result);
-                setVideos(result.items)
-            })
-            .catch(error => console.log(error));
+        setVideos([]);
+        fetchVidoes(searchId);
+        setLoading(true);
     }, [searchId]);
+
+    const fetchVidoes = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&q=${searchId}&pageToken=${pageToken}`)
+            .then((data) => {
+                setNextPageToken(data.nextPageToken);
+                setVideos((preVideos) => [...preVideos, ...data.items]);
+            })
+            .catch((error) => {
+                console.log('Error fetching data', error);
+                setLoading(false);
+            })
+    }
+
+    const handelLoadMore = () => {
+        if (nextPageToken) {
+            fetchVidoes(searchId, nextPageToken);
+        }
+    }
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main
-            title="유투브 검색"
+            title="유튜브 검색"
             description="유튜브 검색 결과 페이지입니다.">
 
-            <section id='searchPage'>
+            <section id='searchPage' className='searchPageClass'>
                 <div className="video__inner search">
                     <VideoSearch videos={videos} />
+                </div>
+                <div className='video__more'>
+                    {nextPageToken && (
+                        <button onClick={handelLoadMore}>더보기</button>
+                    )}
                 </div>
             </section>
         </Main>
