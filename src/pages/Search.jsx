d996a43d815c34ea9792ef1a4699b2cc69568551
Username <parkss0157@naver.com>
2024-02-11 19:03:48 +0900

    버그 고치기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -14,7 +14,7 @@ const Search = () => {
     useEffect(() => {
         setVideos([]);
         fetchVidoes(searchId);
-        setLoading(true);
+        setLoading(false);
     }, [searchId]);
 
     const fetchVidoes = (query, pageToken = '') => {
